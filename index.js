0fd1d9da62e9636760b3b71b884086d855a8ff27
Ray <rjmaestas1991@gmail.com>
2022-10-23 12:21:11 -0600

    commit tree 8f767f8e7d179134dc0915773fda54eed6b5f5c7

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const remove= document.querySelector('main#main')
+remove.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory')
+
+
+newHeader.innerHTML = 'Ray is the champion'
